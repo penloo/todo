1aaffe3ae645ddab1f0512f4d17eff64bde3d4a6
jaemin <penloo@naver.com>
2024-11-13 12:57:58 +0900

    Setting: template setting, feat:page layout, To-do box create

---
+++ my-app/src/app/pages/HomePage/index.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ff0000;
+`;
+
+const Box = styled.div`
+  width: 400px;
+  height: 600px;
+  background-color: #fff;
+  border-radius: 30px;
+  box-shadow: 0 25px 100px -60px rgba(0, 0, 0, 0.18);
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 15px 25px;
+  font-size: 24px;
+  font-weight: 700;
+  /* background-color: #0055ff;
+  color: #fff; */
+`;
+
+const TodoList = styled.div``;
+
+const TodoItem = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 25px;
+  font-size: 1.2em;
+  border-bottom: 1px solid #f0f0f0;
+`;
+const TodoCheck = styled.input`
+  margin-right: 15px;
+`;
+
+export function HomePage() {
+  return (
+    <>
+      <Helmet>
+        <title>Main</title>
+        <meta name="description" content="Todo Main App" />
+      </Helmet>
+      <Wrapper>
+        <Box>
+          <Title>To do List</Title>
+          <TodoList>
+            <TodoItem>
+              <TodoCheck type="checkbox" />
+              <span>하재민</span>
+              <button>Done</button>
+            </TodoItem>
+            <TodoItem>
+              <TodoCheck type="checkbox" />
+              <span>뭘봐</span>
+              <button>Done</button>
+            </TodoItem>
+            <TodoItem>
+              <TodoCheck type="checkbox" />
+              <span>흥</span>
+              <button>Done</button>
+            </TodoItem>
+          </TodoList>
+        </Box>
+      </Wrapper>
+    </>
+  );
+}
